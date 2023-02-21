f19a0d09947204e820fbe3fdd9d7a78a25e8007d
Naresh <nareshkadambini@gmail.com>
2023-02-21 16:17:45 -0500

    Banking System update

--- OOP/Banking System/run.py
+++ OOP/Banking System/run.py
@@ -1,3 +1,7 @@
-import Users_Details
-Naresh = Users_Details.User('Naresh',39,'Male')
+import Bank
+Naresh = Bank.Banks('Naresh',39,'Male')
 Naresh.show_details()
+Naresh.deposit(1000)
+
+Naresh.withdraw(1100)
+Naresh.withdraw(800)
