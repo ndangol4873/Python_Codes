d63b02e1a09e02faf4e60ffad45f18bf78122b5e
Naresh <nareshkadambini@gmail.com>
2023-02-22 18:29:33 -0500

    update

---
+++ OOP/Slot/checkin.py
@@ -0,0 +1,28 @@
+
+from guest import User
+import re
+
+
+
+def user_checkin(option):
+    if option > 0:
+        name = input("Please enter your Full Name : ")
+        if re.match("^[a-zA-Z]+(?:\s[a-zA-Z]+)?$", name):
+            age = input("Enter your age: ")
+            if age.isdigit():
+                age = int(age)
+                gender = input("Enter your gender: ")
+                if re.match("^[A-Za-z]+$", gender):
+                    user_input = User(name, age, gender)
+                    Name,Age,Gender = user_input.show_details()
+                    print(f"Client Registration Details : \n{Name}\n{Age}\n{Gender}")
+            else:
+                print("ERROR Input must be Number")
+
+        else:
+            print("ERROR Input must be Text")
+    
+    else:
+        print('Thank you')
+   
+
---
+++ OOP/Slot/guest.py
@@ -0,0 +1,38 @@
+## Parent class "User"
+# Holds details about the Users
+
+class User():
+    def __init__(self,name,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.balance = 0
+        
+
+    def show_details(self):
+        return self.name, self.age, self.gender
+        # print (f"User Details\nName: {self.name}\nage: {self.age}\ngender: {self.gender} ")
+
+
+    def user_deposit(self, amount):
+        try:
+            self.amount = int(amount)
+        except Exception as e:
+            print("PLEASE ENTER NUMBER")
+
+    
+        if self.amount > self.balance:
+            self.balance = self.balance + self.amount
+            print(f"YOUR BALANCE IS :{self.balance} ")
+        else:
+            print(F"Do NOT HAVE SUFFICIENT BALANCS: {self.balance} ")
+
+    def check_balance(self):
+        print(f"YOUR CURRENCE BALANCE :{self.balance} ")
+
+
+
+# Naresh = User('Naresh', 39,'Male')
+# Naresh.show_details()
+# Naresh.user_deposit(10)
+# Naresh.check_balance()
