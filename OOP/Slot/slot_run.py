3aeffe8a62a56d4785352493c23c1427d67c15f8
Naresh <nareshkadambini@gmail.com>
2023-02-22 18:29:16 -0500

    File add and update in slot

---
+++ OOP/Slot/slot_run.py
@@ -0,0 +1,8 @@
+from guest import User
+from checkin import user_checkin
+
+
+print("Welcome to Slot Game")
+cutomer_option = int(input("Press any number to Continue "))
+
+user_checkin(option=cutomer_option)
