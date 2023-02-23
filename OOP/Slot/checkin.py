224f072692d3a02522ee06889058c38775125a9f
Naresh <nareshkadambini@gmail.com>
2023-02-23 13:23:51 -0500

    file add

--- OOP/Slot/checkin.py
+++ OOP/Slot/checkin.py
@@ -2,23 +2,21 @@ from guest import User
 import re
 
 def user_check_in(option):
-    if option > 0:
-        name = input("Please enter your Full Name : ")
-        if re.match("^[a-zA-Z]+(?:\s[a-zA-Z]+)?$", name):
-            age = input("Enter your age: ")
-            if age.isdigit():
-                age = int(age)
-                gender = input("Enter your gender: ")
-                if re.match("^[A-Za-z]+$", gender):
-                    user_input = User(name, age, gender)
-                    Name,Age,Gender = user_input.show_details()
-                    print(f"Client Registration Details : \n{Name}\n{Age}\n{Gender}")
-            else:
-                print("ERROR Input must be Number")
+    # if option > 0:
+    name = input("Please enter your Full Name : ")
+    if re.match("^[a-zA-Z]+(?:\s[a-zA-Z]+)?$", name):
+        age = input("Enter your age: ")
+        if age.isdigit():
+            age = int(age)
+            gender = input("Enter your gender: ")
+            if re.match("^[A-Za-z]+$", gender):
+                user_input = User(name, age, gender)
+                Name,Age,Gender = user_input.show_details()
+                print(f"Client Registration Details : \n{Name}\n{Age}\n{Gender}")
         else:
-            print("ERROR Input must be Text")
+            print("ERROR Input must be Number")
     else:
-        print('Thank you')
+        print("ERROR Input must be Text")
 
     return Name, Age, Gender
 
