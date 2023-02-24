cc8c2fb0201a11f0cd05002945acecb0a161c2a3
Naresh <nareshkadambini@gmail.com>
2023-02-24 10:40:38 -0500

    file add

--- OOP/Slot/checkin.py
+++ OOP/Slot/checkin.py
@@ -13,11 +13,23 @@ def user_check_in(option):
                 user_input = User(name, age, gender)
                 Name,Age,Gender = user_input.show_details()
                 print(f"Client Registration Details : \n{Name}\n{Age}\n{Gender}")
+                user_balance = user_input.check_balance()
         else:
             print("ERROR Input must be Number")
     else:
         print("ERROR Input must be Text")
 
-    return Name, Age, Gender
+    return Name, Age, Gender,user_balance
 
+
+
+def user_deposit(balance):
+    try:
+        amount = input("Add Balance to Your Account ")
+        amount = int(amount)
+    except Exception as e:
+        print("PLEASE ENTER NUMBER")
+    if amount > balance:
+        balance = balance + amount
+        print(f"YOUR BALANCE IS :{balance} ")
 
--- OOP/Slot/slot_run.py
+++ OOP/Slot/slot_run.py
@@ -1,10 +1,10 @@
 from guest import User
-from checkin import user_check_in
+from checkin import user_check_in, user_deposit
 
 ## PROGRAM RUN SCRIPT
 
 print("Welcome to Slot Game")
 customer_option = int(input("Press any number to Continue "))
-Name,Age,Gender = user_check_in(option=customer_option)
+Name,Age,Gender,Balance = user_check_in(option=customer_option)
 
-
+user_deposit(Balance)
