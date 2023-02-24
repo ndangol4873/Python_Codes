3b6ce0abe96e0f153747fa4a2e010f0666c6650b
Naresh <nareshkadambini@gmail.com>
2023-02-24 15:44:02 -0500

    script update

--- OOP/Slot/checkin.py
+++ OOP/Slot/checkin.py
@@ -36,3 +36,36 @@ def user_deposit(balance):
         print(f"YOUR BALANCE IS : $ {balance} ")
         return balance
 
+
+
+MAX_LINES = 3
+def get_number_of_lines():
+    while True:
+       lines = input("Enter number of lines to bet on (1-" +str(MAX_LINES) + ")? ")
+       if lines.isdigit():
+           lines = int(lines)
+           if 1 <= lines <= MAX_LINES:
+               break
+           else:
+               print("Enter valid number of lines ")
+       else:
+           print("Please enter number")
+    return  lines
+
+
+#             print("Please enter a number ")
+MAX_BET = 100
+MIN_BET = 1
+
+def get_bet():
+    while True:
+       amount = input("What would you like to bet on each line? ")
+       if amount.isdigit():
+           amount = int(amount)
+           if MIN_BET <= amount <= MAX_BET:
+               break
+           else:
+               print(f"Amount must be between  $ { MAX_BET} -$  {MAX_BET}. ")
+       else:
+           print("Please enter number")
+    return  amount
